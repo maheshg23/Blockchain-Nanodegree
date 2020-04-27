e0661c8e1d1127f5c0fe1ca5ff87cc195061150f
Mahesh Gopinathan <gmn1994@gmail.com>
2020-04-27 14:23:44 -0400

    Remove Keys and Addresses

--- myLocalBlockchain/transaction_rinkeby.js
+++ myLocalBlockchain/transaction_rinkeby.js
@@ -4,12 +4,12 @@
 var Web3 = require("web3") 
 // var EthereumTransaction = require("ethereumjs-tx") 
 var EthereumTransaction = require('ethereumjs-tx').Transaction;
-var web3 = new Web3('https://rinkeby.infura.io/v3/264c6913a5524f61ab70544cac16a198')
+var web3 = new Web3('RinkeBy URL')
 // console.log(web3)
 
 // -- Step 2: Set the sending and receiving addresses for the transaction. 
-var sendingAddress = '0xc677420923196099E2e3feF15761dde98F3c90FF' 
-var receivingAddress = '0xF66275976A1f61b2B4a20406bCADc1Ad7853928c'
+var sendingAddress = 'ADDRESS1' 
+var receivingAddress = 'ADDRESS2'
 
 // -- Step 3: Check the balances of each address 
 web3.eth.getBalance(sendingAddress).then(console.log) 
@@ -41,7 +41,7 @@ web3.eth.getBalance(receivingAddress).then(console.log)
 //####### Sign the Transaction #######
 
 // -- Step 7: Sign the transaction with the Hex value of the private key of the sender 
-var privateKeySender = '73A21AA39730E4476876581BB71B1660E42CA84C70AE20E070CC31130A9A6B3D' 
+var privateKeySender = 'PRIVATEKEY' 
 var privateKeySenderHex = Buffer.from(privateKeySender, 'hex') 
 var transaction = new EthereumTransaction(rawTransaction,{chain:'ropsten'}) 
 transaction.sign(privateKeySenderHex)
